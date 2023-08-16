42a64bee17f9ef1b96604595b0d30c430d498aad
kongsopheaktra96 <kongsopheaktra96@gmail.com>
2023-08-16 11:43:13 +0700

    setup Vue3  with Tailwindcss

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,84 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+    <div>
+      <h2>Products:</h2>
+      <div>
+        <label for="searchId">Search by ID:</label>
+        <input type="number" id="searchId" v-model="searchId" />
+        <button @click="searchById">Search</button>
+      </div>
+      <div>
+        <label for="searchCategory">Search by Category:</label>
+        <select id="searchCategory" v-model="searchCategory">
+          <option value="">All</option>
+          <option v-for="category in categories" :value="category.id" :key="category.id">
+            {{ category.name }}
+          </option>
+        </select>
+        <button @click="searchByCategory">Search</button>
+      </div>
+      <ul>
+        <li v-for="product in filteredProducts" :key="product.id">
+          {{ product.name }} (Category: {{ getCategoryName(product.category_id) }})
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        products: [
+          { id: 1, name: 'Dell', category_id: 101 },
+          { id: 2, name: 'Power Supply', category_id: 102 },
+          { id: 3, name: 'Macbook', category_id: 101 },
+          { id: 4, name: 'Monitor', category_id: 103 },
+          { id: 5, name: 'Keyboard', category_id: 104 },
+          { id: 6, name: 'Mouse', category_id: 104 },
+          { id: 7, name: 'Headphones', category_id: 105 },
+        ],
+        categories: [
+          { id: 101, name: 'Computers' },
+          { id: 102, name: 'Power Supplies' },
+          { id: 103, name: 'Monitors' },
+          { id: 104, name: 'Mice and Keyboards' },
+        ],
+        filteredProducts: [],
+        searchId: null,
+        searchCategory: null,
+      };
+    },
+    computed: {
+      getCategoryName() {
+        return function (categoryId) {
+          const category = this.categories.find((category) => category.id === categoryId);
+          return category ? category.name : '';
+        };
+      },
+    },
+    methods: {
+        searchById() {
+            this.filteredProducts = [];
+            for (let i = 0; i < this.products.length; i++) {
+            const product = this.products[i];
+            if (product.id === this.searchId) {
+                this.filteredProducts.push(product);
+            }
+            }
+        },
+        searchByCategory() {
+            this.filteredProducts = [];
+            if (this.searchCategory) {
+            for (let i = 0; i < this.products.length; i++) {
+                const product = this.products[i];
+                if (product.category_id === this.searchCategory) {
+                this.filteredProducts.push(product);
+                }
+            }
+            } else {
+            this.filteredProducts = this.products;
+            }
+        },
+        },
+  };
+  </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,99 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <div class="font-bold">1. Display product information</div>
+      <input type="number" style="border: 1px solid grey" v-model="productID" />
+      <button
+        style="background-color: green; border-radius: 10px"
+        class="ml-5 p-1 text-white"
+        @click="displayProduct"
+      >
+        Search Product
+      </button>
+    </div>
+    <div class="flex justify-center">
+      <div style="border: 1px solid green" class="mt-2 w-96 h-96">
+        {{ ProductSearched }}
+      </div>
+    </div>
+
+    <br />
+    <div>
+      <div>
+        <div class="font-bold">2. Display Related Product</div>
+        <input
+          type="number"
+          style="border: 1px solid grey"
+          v-model="category"
+        />
+        <button
+          style="border-radius: 10px; background-color: green"
+          class="p-1 ml-5 text-white"
+          @click="searchRelatedProduct"
+        >
+          Search Related Product
+        </button>
+      </div>
+      <div class="flex justify-center">
+        <div style="border: 1px solid green" class="mt-2 w-96 h-96">
+          {{ relatedProducts }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      products: [
+        { id: 1, name: "Dell", category_id: 101 },
+        { id: 2, name: "Power Supply", category_id: 102 },
+        { id: 3, name: "Macbook", category_id: 101 },
+        { id: 4, name: "Monitor", category_id: 103 },
+        { id: 5, name: "Keyboard", category_id: 104 },
+        { id: 6, name: "Mouse", category_id: 104 },
+        { id: 7, name: "Headphones", category_id: 105 },
+      ],
+      categories: [
+        { id: 101, name: "Computers" },
+        { id: 102, name: "Power Supplies" },
+        { id: 103, name: "Monitors" },
+      ],
+      ProductList: [],
+      productID: "",
+      ProductSearched: [],
+      category: "",
+
+      // Question 2
+      relatedProducts: [],
+    };
+  },
+  created() {
+    this.displayProduct();
+    this.searchRelatedProduct();
+  },
+  methods: {
+    displayProduct() {
+      this.ProductList = this.products;
+      console.log("List Products: ", this.ProductList);
+      for (let i = 0; i < this.ProductList.length; i++) {
+        if (parseInt(this.ProductList[i].id) === this.productID) {
+          this.ProductSearched.push(this.ProductList[i]);
+        }
+      }
+    },
+    searchRelatedProduct() {
+      for (let i = 0; i < this.ProductList.length; i++) {
+        if (parseInt(this.ProductList[i].category_id) === this.category) {
+          return this.relatedProducts.push(this.ProductList[i]);
+        }
+      }
+      console.log("Related Product:", this.relatedProducts);
+    },
+  },
+};
 </script>
+
+<style scoped></style>
